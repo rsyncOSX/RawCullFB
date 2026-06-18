00174da2b97c9c2bb6d2064abb10c73d04999bdf
Thomas Evensen <thomeven@gmail.com>
2026-06-18 18:53:42 +0200

    update

---
+++ RawCullFB/Model/JSON/DecodeSavedFiles.swift
@@ -0,0 +1,53 @@
+//
+//  DecodeSavedFiles.swift
+//  RawCull
+//
+//  Created by Thomas Evensen on 27/01/2026.
+//
+
+import Foundation
+import RawCullCore
+
+struct DecodeSavedFiles: Codable {
+    let catalog: URL?
+    let dateStart: String?
+    var filerecords: [DecodeFileRecord]?
+   
+    enum CodingKeys: String, CodingKey {
+        case catalog
+        case dateStart
+        case filerecords
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        catalog = try values.decodeIfPresent(URL.self, forKey: .catalog)
+        dateStart = try values.decodeIfPresent(String.self, forKey: .dateStart)
+        filerecords = try values.decodeIfPresent([DecodeFileRecord].self, forKey: .filerecords)
+    }
+}
+
+struct DecodeFileRecord: Codable {
+    var fileName: String?
+    var dateTagged: String?
+    var rating: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case fileName
+        case dateTagged
+        case rating
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        fileName = try values.decodeIfPresent(String.self, forKey: .fileName)
+        dateTagged = try values.decodeIfPresent(String.self, forKey: .dateTagged)
+        rating = try values.decodeIfPresent(Int.self, forKey: .rating)
+    }
+
+    init() {
+        fileName = nil
+        dateTagged = nil
+        rating = nil
+    }
+}
